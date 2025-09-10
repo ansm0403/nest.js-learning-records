a0e5e66d69d8734e43d13972d4dc5773315e396b
an-sangmoon <tkdans312@gmail.com>
2025-09-10 23:32:07 +0900

    feat: JWT set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { PostsModule } from './posts/posts.module';
 import { PostsModel } from './posts/entities/posts.entity';
 import { UsersModule } from './users/users.module';
 import { UserModel } from './users/entities/users.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { UserModel } from './users/entities/users.entity';
     }),
     PostsModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
